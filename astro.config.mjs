a0a45febbc525c4ddebe449bfa866a7a6d007b7e
stefanovidmar2005 <stefanovidmar@gmail.com>
2024-06-25 01:28:22 +0300

    agency website

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig, passthroughImageService } from "astro/config";
+import icon from "astro-icon";
+import { register } from "swiper/element/bundle";
+
+register();
+
+export default defineConfig({
+  integrations: [icon()],
+  image: {
+    service: passthroughImageService(),
+  },
+});
