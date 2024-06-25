df74c534176fe6eb5d48ef09779e80d41d8e29e1
stefanovidmar2005 <stefanovidmar@gmail.com>
2024-06-25 13:00:54 +0300

    updated navigation issue in desktop

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig, passthroughImageService } from "astro/config";
 import icon from "astro-icon";
 import { register } from "swiper/element/bundle";
+import react from "@astrojs/react";
 
 register();
 
 export default defineConfig({
-  integrations: [icon()],
+  integrations: [react(), icon()],
   image: {
     service: passthroughImageService(),
   },
